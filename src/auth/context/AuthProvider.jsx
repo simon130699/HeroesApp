f420ee480e671a104baa920b11e4d773bceb62e6
Simon Perez <simon130699@gmail.com>
2024-07-04 15:28:27 -0300

    cambios en parte publica y privada

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,15 +1,40 @@
 import React, { useReducer } from 'react'
 import { AuthContext } from './AuthContext';
 import { authReducer } from './AuthReducer';
-const initialState = {
-    logged: false,
+import { types } from '../types/types';
+
+const init =()=>{
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,//si el valor de logged es NULL retorna true
+    user:user
+  }
 }
 export const AuthProvider = ({children}) => {
 
-const [state,dispatch]=useReducer(authReducer,initialState);
+const [authState,dispatch]=useReducer(authReducer,{}, init);
 
+const login=(name='')=>{
+const user = {id: 'abc', name}
+  const action = {type: types.login, payload: user}
+
+  localStorage.setItem('user',JSON.stringify(user));
+
+  dispatch(action);
+}
+const logout = ()=>{
+  localStorage.removeItem('user');
+  const action = {type: types.logout};
+  dispatch(action);
+}
   return (
- <AuthContext.Provider value={{}}>
+ <AuthContext.Provider value={{
+  ...authState, 
+  login:login,
+  logout:logout
+}}
+ >
     {children}
  </AuthContext.Provider>
   );
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
+    const{user,logout}= useContext(AuthContext);
 
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout();
         navigate('/login', {
             replace: true
         });
@@ -51,7 +55,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
                    
                     <span className="nav-item nav-link text-primary">
-                        Simon
+                        {user?.name}
                     </span>
 
                     <button
